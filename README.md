de5c83a9a2bc036988fc0dcba6b5441f702d3f42
JhowAmaya01 <jhohanaamaya1e@gmail.com>
2023-09-12 11:40:12 -0600

    Actividad 5

--- README.md
+++ README.md
@@ -7,14 +7,20 @@
 - p01-hola-mundo
 - p02-paga-trabajador
 - p03-venta-pizzas
+## Actividad 2
 - p04-procesa-calificaciones
 - p05-tabla-conversion
 - p06-factorial-numeros
+## Actividad 3
 - p07-estadisticas
+## Tarea 1
 - p08-distancia-puntos 
 - p09-volumen-esfera
 - p10-calificaciones
 - p11-venta-articulos
 - p12-numeros-arreglo 
+## Actividad 4
 - p13-empleado
 - p14-control-bancario-v1
+## Actividad 5
+- p15-control-bancario-v2
---
+++ p15-control-bancario-v2/Program.cs
@@ -0,0 +1,39 @@
+﻿//Control bancario v2
+
+
+//cremos un banco
+Banco mibanco = new Banco ("Banco de Jhohana", "Mi casa #105");
+
+//Agregamos dos clientes al banco (Forma 1)
+mibanco.AgregarCliente(new Cliente("Carlos Fuentes", "cfuentes@gmail.com", new List<CuentaBancaria>{
+    new CuentaDeAhorro(1500,0.10), new CuentaDeAhorro(2500,0.20), new CuentaDeCheques(6500,500)}));
+mibanco.AgregarCliente(new Cliente("Juan de la Fuente","juanfuente@gmail.com", new List<CuentaBancaria>{
+    new CuentaDeCheques(2500,500)}));
+
+//Agregamos dos clientes al banco (Forma 2)
+mibanco.AgregarCliente(new Cliente("Maria Perez", "mperez@gmail.com", new List<CuentaBancaria>()));
+mibanco.Clientes[2].AgregarCuenta(new CuentaDeCheques(16500,1500));
+mibanco.Clientes[2].AgregarCuenta(new CuentaDeAhorro(116500,0.40));
+mibanco.AgregarCliente(new Cliente("Petra Dominguez", "pdom@gmail.com", new List<CuentaBancaria>()));
+mibanco.Clientes[3].AgregarCuenta(new CuentaDeAhorro(12500,0.30));
+
+//retiramos y depositamos a las diferentes cuentas de algunos clientes
+mibanco.Clientes[1].Cuentas[0].Retira(3000);
+mibanco.Clientes[2].Cuentas[0].Retira(17000);
+mibanco.Clientes[2].Cuentas[0].Retira(1000);
+mibanco.Clientes[3].Cuentas[0].Deposita(200);
+
+//Reporte de clientes
+Console.WriteLine($"Reporte de clientes del banco \n {mibanco.ToString()}");
+Console.WriteLine($"Clientes: {mibanco.Clientes.Count}\n");
+
+//Imprimir los clientes y sus respectivos datos
+foreach(Cliente cte in mibanco.Clientes){
+    Console.Write($"\nCliente:\n{cte.ToString()}, cuentas: {cte.Cuentas.Count}\n");
+    foreach(CuentaBancaria cta in cte.Cuentas){
+        Console.Write((cta is CuentaDeCheques)?"Cuenta de cheques":"Cuenta de ahorro");
+        if(cta is CuentaDeAhorro)
+            (cta as CuentaDeAhorro).CalcularInteres();
+            Console.Write($", con un saldo de {cta.Saldo:n2}\n");
+    }
+}
